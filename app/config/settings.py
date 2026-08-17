1b36b00526ed14a37199b744b596a329b26754c4
SarahGolabvand <s.golabvand1@gmail.com>
2026-08-17 22:18:19 +0330

    conf(set poetry): set some configs about poetry

--- app/config/settings.py
+++ app/config/settings.py
@@ -145,3 +145,16 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "core.User"
+
+LOCALE_PATHS = [
+    BASE_DIR / "locale",
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
