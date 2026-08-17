ff0147d67ef884a277c27584f3c166ad5fb22b8a
SarahGolabvand <s.golabvand1@gmail.com>
2026-08-17 22:24:38 +0330

    feat(contact section )
    -  Form
    - Admin
    - model
    -urls
    -view
    -templates

--- app/config/urls.py
+++ app/config/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
     path("",include('core.urls')),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('contact/', include('contact.urls')),
 ]
---
+++ app/contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from .models import ContactMessage
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ["name", "email", "subject", "message"]
+    name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(required=True)
+    subject = forms.CharField(max_length=150, required=True)
+    message = forms.CharField(widget=forms.Textarea)
