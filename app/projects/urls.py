37706e7875511941b81c41cf65b60aba53dadff9
SarahGolabvand <s.golabvand1@gmail.com>
2026-08-17 22:27:22 +0330

    clean (project)
    clean and order project section templates

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 app_name = 'projects'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='projects.html'), name='projects'),
-    path('single',TemplateView.as_view(template_name='project-detail.html'), name='project_detail')
+    path('', TemplateView.as_view(template_name='projects_list/projects.html'), name='projects'),
+    path('single',TemplateView.as_view(template_name='project_detail/project_detail.html'), name='project-detail')
 
 ]
