c6fc6e77d6278bd20d29c48dc87ea47e3c3d40d7
SarahGolabvand <s.golabvand1@gmail.com>
2026-09-03 03:31:12 +0330

    fix(project model)
    fix one to many relations
    Tag, KeyOutput and stacks with Project Model

--- app/projects/models.py
+++ app/projects/models.py
@@ -3,44 +3,20 @@ from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-
-
-class Tag(models.Model):
-    name = models.CharField(_("tag's name"), max_length=50)
-    display_order = models.PositiveIntegerField(default=0)
-
-    class Meta:
-        ordering = ['display_order', 'id']
-
-    def __str__(self):
-        return self.name
-
-
 class Project(models.Model):
-    tag = models.ForeignKey(
-        'Tag',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='projects',
-    )
 
-    title = models.CharField(_("project's name"), max_length=100)
     slug = models.SlugField(_('slug'), max_length=255, unique=True, blank=True)
-    description = models.CharField(_("description"), max_length=250)
-
-    overview_title = models.CharField(_("article_title"), max_length=250)
-
+    thumbnail = models.ImageField(_("thumbnail"),
+                                  upload_to='project/thumbnails', null=True, blank=True)
+    name = models.CharField(_("project's name"), max_length=100)
+    short_description = models.CharField(_("short description"),
+                                         max_length=250)
+    article_title = models.CharField(_("Article Title"), max_length=250)
+    article = models.TextField(
+        _("Article"), help_text='split paragraphs with an Enter')
     role = models.CharField(_("Role"), max_length=50)
     timeline = models.CharField(_("timeline"), max_length=50)
-
     demo_url = models.URLField(_("demo url"), blank=True)
-
-    # screenshots -> FK
-    # key_outcomes -> FK
-    # stack_items -> FK
-    # overview_paragraphs -> FK
-
     is_published = models.BooleanField(_(" is published"), default=False)
     display_order = models.PositiveIntegerField(_("display order"), default=0)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -48,30 +24,25 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['display_order', '-created_at']
+        db_table = 'projects'
+
+    @property
+    def paragraphs(self):
+        return self.article.split("\n\n")
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
-
-
-class ProjectOverviewParagraph(models.Model):
-    project = models.ForeignKey(
-        Project, on_delete=models.CASCADE, related_name='overview_paragraphs')
-    text = models.TextField()
-    display_order = models.PositiveIntegerField(default=0)
-
-    class Meta:
-        ordering = ['display_order', 'id']
-
-    def __str__(self):
-        return f'{self.project.title} paragraph {self.display_order}'
+        return self.name
 
 
 class ProjectScreenshots(models.Model):
+    class Meta:
+        ordering = ['display_order', 'id']
+        db_table = 'projects_screenshots'
     project = models.ForeignKey("Project", verbose_name=_(
         "project"), on_delete=models.CASCADE, related_name='screenshots')
     image = models.ImageField(_("screenshot"), upload_to='projects/screenshots/',
@@ -81,32 +52,51 @@ class ProjectScreenshots(models.Model):
     is_primary = models.BooleanField(_("is primary"))
     display_order = models.PositiveIntegerField(_("display order"), default=0)
 
-    class Meta:
-        ordering = ['display_order', 'id']
+    project_name = ''
+
+    @property
+    def get_project_name(self):
+        self.project_name = self.project.name
 
     def __str__(self):
-        return f'{self.project.title} screenshot'
+        return f'{self.project.name} screenshot'
 
 
 class ProjectKeyOutcome(models.Model):
-
+    class Meta:
+        ordering = ['display_order', 'id']
+        db_table = 'projects_key_outcomes'
     project = models.ForeignKey("Project", verbose_name=_(
         "project's tags"), on_delete=models.CASCADE, related_name='key_outcomes')
 
     key_outcomes = models.CharField(_("key_outcomes"), max_length=250)
     display_order = models.PositiveIntegerField(default=0)
 
+    project_name = ''
+
+    @property
+    def get_project_name(self):
+        self.project_name = self.project.name
+
+    def __str__(self):
+        return f"{self.project.name}'s key outcomes"
+
+class ProjectStackItem(models.Model):
     class Meta:
         ordering = ['display_order', 'id']
+        db_table = 'project_stack_items'
+    project = models.ForeignKey("Project", verbose_name=_(
+        "project's stacks"), on_delete=models.CASCADE, related_name='stack_items')
+    name = models.CharField(max_length=50)
+    display_order = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
 
-
-class ProjectStackItem(models.Model):
+class Tag(models.Model):
     project = models.ForeignKey(
-        Project, on_delete=models.CASCADE, related_name='stack_items')
-    name = models.CharField(max_length=50)
+        'Project', related_name='tags', on_delete=models.CASCADE)
+    name = models.CharField(_("tag's name"), max_length=50)
     display_order = models.PositiveIntegerField(default=0)
 
     class Meta:
