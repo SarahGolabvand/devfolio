2b04e67162494848a83c445f3fd47bc623fb63bb
SarahGolabvand <s.golabvand1@gmail.com>
2026-09-12 17:25:02 +0330

    created Core App

--- app/core/translation.py
+++
@@ -1,9 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-from .models import SkillsModel, ProfileModel
-
-@register(SkillsModel)
-class SkillsTranslationOptions(TranslationOptions):
-    fields = ('description') 
-@register(ProfileModel)
-class ProfileTranslationOptions(TranslationOptions):
-    fields = ('first_name','last_name','bio')
--- app/core/views.py
+++ app/core/views.py
@@ -1,21 +1,3 @@
+from django.shortcuts import render
 
-from .models import SkillsModel, ProfileModel
-from django.views.generic.base import TemplateView
-from projects.models import Project
-
-
-class IndexView(TemplateView):
-    template_name = 'index/index.html'
-    
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "skills": SkillsModel.objects.filter(is_visible=True).order_by("order", "title"),
-            'profile': ProfileModel.objects.first(),
-            "projects": (Project.objects.filter(is_published=True)
-                         .prefetch_related('tags')
-                         .order_by('-created_at'))[:3]
-
-        })
-        return context
+# Create your views here.
